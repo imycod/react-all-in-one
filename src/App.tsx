d5ea59928f524030a7293af579509ec1f9339f89
wuxs <317009160@qq.com>
2023-09-29 02:03:24 +0800

    feat: pass props children html

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,9 @@
-/*
- * @Author: wuxs 317009160@qq.com
- * @Date: 2023-09-29 00:37:04
- * @LastEditors: wuxs 317009160@qq.com
- * @LastEditTime: 2023-09-29 01:51:28
- * @FilePath: \react-all-in-one\src\App.tsx
- * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
- */
-import ListGroup from "./components/ListGroup";
+import Alert from "./components/Alert";
 
 function App() {
-  let items = ["New York", "Los Angeles", "Chicago", "Houston", "Philadelphia"];
-
-  const handleSelectItem = (item: string) => {
-    console.log(item);
-  };
   return (
     <div>
-      <ListGroup
-        items={items}
-        heading="Cities"
-        onSelectItem={handleSelectItem}
-      />
+      <Alert>Hello <span>World</span></Alert>
     </div>
   );
 }
