6e754180059f015dea0d5c7f6dcd181f3bc30d44
wuxs <317009160@qq.com>
2023-11-04 14:32:30 +0800

    docs:用jotai可以避免context层层嵌套问题

--- src/useContext/NestingContext/Demo1.tsx
+++ src/useContext/NestingContext/Demo1.tsx
@@ -4,58 +4,57 @@ import {atom,useAtom} from "jotai"
 const UserInfo = createContext({})
 const Role = createContext({})
 
-const UserContextAtom = atom({})
+const UserContextAtom = atom({
+    userInfo:{
+        name:'ss'
+    },
+    role:'admin'
+})
 function useUserContextAtom() {
     return useAtom(UserContextAtom)
 }
 
 function UserContainer() {
-    const [user,setUser]=useState({
-        userInfo: {
-            name: 'bill'
-        },
-        role: "admin"
-    })
-
-   function setRole(role){
-       setUser((prev) => ({
-           ...prev,
-           role: role
-       }));
-    }
-
-    const roleContext=useMemo(()=>({role:user.role,setRole}),[user.role,setRole])
     return (
         <div className="bd">
-            <UserInfo.Provider value={user.userInfo}>
-                <Role.Provider value={roleContext}>
-                    <>
-                        <span>最外层提供Provider父级 {Math.random()}</span>
-                        <UserCardContainer></UserCardContainer>
-                    </>
-                </Role.Provider>
-            </UserInfo.Provider>
+            <span>最外层提供Provider父级 {Math.random()}</span>
+            <UserCardContainer></UserCardContainer>
         </div>
     );
 }
 
-const UserCardContainer = memo(()=>{
+const UserCard=()=>{
+    const [context]= useUserContextAtom()
+    const {userInfo}=context
+    return (
+        <div className="bd" style={{display:'flex',}}>
+            <span>组件:B {userInfo.name} {Math.random()}</span>
+            <RoleComponent></RoleComponent>
+            <SavingInfo></SavingInfo>
+        </div>
+    )
+}
+
+const RoleComponent=  function (){
+    const [context,setContext]=useUserContextAtom()
+    const {userInfo}=context
+    const role = context.role;
+
+    return (
+        <div className="m-2 bd">
+            <div>组件B1: {userInfo.name} - {role}</div>
+            <input type="text" onChange={(event) => setContext((prev)=>({...prev,role:event.target.value}))}/>
+            <span>组件C: {Math.random()}</span>
+        </div>
+    );
+}
+const UserCardContainer = ()=>{
     return (
         <div className="bd">
             <span> 组件A: {Math.random()}</span>
             <UserCard></UserCard>
         </div>
     );
-})
-function UserCard(){
-    const context= useContext(UserInfo)
-    return (
-        <div className="bd" style={{display:'flex',}}>
-            <span>组件:B {context.name} - {Math.random()}</span>
-            <RoleComponent userInfo={context}></RoleComponent>
-            <SavingInfo></SavingInfo>
-        </div>
-    )
 }
 const SavingInfo = memo(() => {
     return (
@@ -65,22 +64,6 @@ const SavingInfo = memo(() => {
         </div>
     )
 });
-const RoleComponent=  function (props){
-    const userInfo = props.userInfo
-    return (
-        <Role.Consumer>
-            {
-                contextValue => (
-                    <div className="m-2 bd">
-                        <div>组件B1: {userInfo.name} - {contextValue.role}</div>
-                        <input type="text" onChange={(event)=>contextValue.setRole(event.target.value)}/>
-                        <span>组件C: {Math.random()}</span>
-                    </div>
-                )
-            }
-        </Role.Consumer>
-    )
-}
 function NestingDemoExample() {
     return (
         <div>
--- src/useContext/NestingContext/index.tsx
+++ src/useContext/NestingContext/index.tsx
@@ -2,11 +2,13 @@ import React from 'react';
 
 import Demo from "./Demo.tsx"
 import Demo1 from "./Demo1.tsx"
+import Demo2 from "./Demo2.tsx"
 function NestingAtomExample() {
     return (
         <div>
             {/*<Demo></Demo>*/}
             <Demo1></Demo1>
+            {/*<Demo2></Demo2>*/}
         </div>
     );
 }
