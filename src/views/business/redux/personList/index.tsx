3d8e346fc6120f333a16ecc72f3a595fbc66a30f
wuxs <317009160@qq.com>
2023-11-07 01:34:46 +0800

    docs:react-redux person和class list table 练习

--- src/views/business/redux/personList/index.tsx
+++ src/views/business/redux/personList/index.tsx
@@ -1,27 +1,13 @@
-import React, {useEffect, useState} from 'react';
+import React, {Component, useEffect, useState} from 'react';
 import {Button, Input, Space, Table} from "antd";
-import {useDispatch, useSelector} from "react-redux";
+import {connect, useDispatch, useSelector} from "react-redux";
 import {ADD_PERSON} from "@/constant/business.tsx";
 import {nanoid} from "nanoid";
 
-function PersonList(props) {
-    const persons = useSelector(state=>state.personReducer)
-    const [dataSource,setDataSource]=useState(persons)
-    const [user,setUser]=useState({
-        name:'',
-        age:''
-    })
-    const dispatch= useDispatch()
 
-    useEffect(()=>{
-        console.log('persons---',persons)
-        setDataSource(persons)
-    },[persons])
+class List extends Component{
 
-    const add=()=>{
-        dispatch({type:ADD_PERSON,data:user})
-    }
-    const columns=[
+    columns=[
         {
             title: '姓名',
             dataIndex: 'name',
@@ -33,6 +19,45 @@ function PersonList(props) {
             key: 'age',
         },
     ]
+    render(){
+        return (
+            <div>
+                <Table  rowKey="id" dataSource={this.props.dataSource} columns={this.columns}></Table>
+            </div>
+        )
+    }
+}
+const mapStateToProps= ({personReducer})=>({dataSource:personReducer})
+const ListContainer = connect(mapStateToProps)(List)
+function PersonList(props) {
+    // const persons = useSelector(state=>state.personReducer)
+    // const [dataSource,setDataSource]=useState(persons)
+    const [user,setUser]=useState({
+        name:'',
+        age:''
+    })
+    const dispatch= useDispatch()
+
+    // useEffect(()=>{
+    //     console.log('persons---',persons)
+    //     setDataSource(persons)
+    // },[persons])
+
+    const add=()=>{
+        dispatch({type:ADD_PERSON,data:user})
+    }
+    // const columns=[
+    //     {
+    //         title: '姓名',
+    //         dataIndex: 'name',
+    //         key: 'name',
+    //     },
+    //     {
+    //         title: '年龄',
+    //         dataIndex: 'age',
+    //         key: 'age',
+    //     },
+    // ]
     function oninput(type) {
         return (e)=>{
             setUser({...user,[type]:e.target.value,id:nanoid()})
@@ -50,9 +75,7 @@ function PersonList(props) {
                 </Space>
                 <Button type={'primary'} onClick={add}>add</Button>
             </div>
-            <div>
-                <Table  rowKey="id" dataSource={dataSource} columns={columns}></Table>
-            </div>
+            <ListContainer></ListContainer>
         </div>
     );
 }
